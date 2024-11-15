cefb1b3dc3984319e341298262aaf9ff0abaef58
Amanda <nanda.hamandadesouza@gmail.com>
2024-11-15 13:14:18 -0300

    Feat: Cadastrar pessoa

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from '../../modelo/Pessoa';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.scss'
 })
@@ -16,4 +18,12 @@ export class FormularioComponent {
     cidade: new FormControl('', [Validators.required, Validators.minLength(3)])
   })
 
+  btnCadastrar: boolean = true;
+
+  vetor: Pessoa[] = []
+
+  cadastrar(){
+    this.vetor.push(this.formulario.value as Pessoa);
+    this.formulario.reset()
+  } 
 }
