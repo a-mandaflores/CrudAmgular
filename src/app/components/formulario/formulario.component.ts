4b306e0803de72062f157e5ef1763b6c3286a194
Amanda <nanda.hamandadesouza@gmail.com>
2024-11-15 12:52:57 -0300

    Feat: Validators

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.scss'
 })
 export class FormularioComponent {
+
+  formulario = new FormGroup({
+    nome : new FormControl('', [Validators.required, Validators.minLength(3)]),
+    idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
+    cidade: new FormControl('', [Validators.required, Validators.minLength(3)])
+  })
 
 }
